fff0d5fea930fe83a13fff27450f9625faa40c19
Alexander Rudetski <39997010+Haniley1@users.noreply.github.com>
2025-03-23 18:43:26 +0300

    init

---
+++ server/server.ts
@@ -0,0 +1,22 @@
+import app from './app';
+import http from 'http';
+import { Server } from 'socket.io';
+
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: '*',
+  },
+});
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+});
+
+const PORT = process.env.PORT || 4000;
+server.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
